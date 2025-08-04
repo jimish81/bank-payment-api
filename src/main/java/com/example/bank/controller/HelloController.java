3ba91fcaa5eb09bde58843d4f2ed18b585171f79
iammanas1975 <140784033+iammanas1975@users.noreply.github.com>
2025-08-04 16:23:13 +0530

    Update HelloController.java

--- src/main/java/com/example/bank/controller/HelloController.java
+++ src/main/java/com/example/bank/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.bankpaymentapi;
+package com.example.bank.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
